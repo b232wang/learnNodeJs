9ce812f12b252caae90c606b24e998cbfecac58f
Steven <wby10319454@gmail.com>
2015-12-08 21:37:45 -0500

    le3 done

--- le3/app.js
+++ le3/app.js
@@ -12,19 +12,34 @@ app.get("/",function(req,res,next){
         }
         var $ = cheerio.load(sres.text);
         var items=[];
-        $('#topic_list .topic_title').each(
+        $('#topic_list ').find('.topic_title').each(
             function(idx, element){
                 var $element = $(element);
+                var href = $element.attr('href');
                 items.push({
                     title: $element.attr('title'),
                     href: $element.attr('href'),
-                    author: $element.attr('author'),
+                    author: $element.parents('.cell').find('img').attr('title')
                 });
+                //superagent.get("https://cnodejs.org"+href)
+                //.end(function(err,ssres){
+                //    var $$ = cheerio.load(ssres.text);
+                //    //theAuthor= $$('#content').find('.changes').eq(1).text();
+                //    var theAuthor= $$('#content .changes span').eq(1).find('a').text();
+                //    items[idx].author = theAuthor;
+                //    console.log(idx+" -> "+theAuthor);
+                //})
             });
             res.send(items);
+            //superagent.get("https://cnodejs.org/topic/565c4473d0bc14ae279399fe")
+            //.end(function(err,uu){
+            //    var uufs = cheerio.load(uu.text);
+            //    var pp = uufs('#content .changes span').eq(1).find('a').text();
+            //    console.log(" -> "+pp);
+            //})
     });
 })
 
 app.listen(3000, function(){
-     console.log("listen 3000");
+    console.log("listen 3000");
 })
